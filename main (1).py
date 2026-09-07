9a1668757d7a2234e2ac4f2d586ab05d64a922cd
Jil L. Tampus <jiltampus12@gmail.com>
2026-09-07 15:26:47 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+#Ask the user for a whole number.
+num = int(input("Enter a whole number: "))
+
+#Check if number is divisible by 2.
+if num % 2 == 0:
+  print("The number is even.")
+else:
+  print("The number is odd.") 
